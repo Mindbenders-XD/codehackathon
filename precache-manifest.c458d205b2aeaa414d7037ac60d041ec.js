854ace1806f927253e43c2a630e937a34ec3c7b9
Janakiraman <Janakiramanc@users.noreply.github.com>
2020-02-17 10:34:21 +0530

    Add files via upload

---
+++ precache-manifest.c458d205b2aeaa414d7037ac60d041ec.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e011742145520d3fa2ba01038226e242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a202bb689a5a245b3d46",
+    "url": "/static/css/main.a4a89779.chunk.css"
+  },
+  {
+    "revision": "b900d5aea79b4e446b37",
+    "url": "/static/js/2.505dcfd9.chunk.js"
+  },
+  {
+    "revision": "45d67251489530ae92636cc6bbfdebb5",
+    "url": "/static/js/2.505dcfd9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a202bb689a5a245b3d46",
+    "url": "/static/js/main.a9f73f81.chunk.js"
+  },
+  {
+    "revision": "354e2675038836d36ab1",
+    "url": "/static/js/runtime-main.97f7d558.js"
+  },
+  {
+    "revision": "8c6c7171460fd6564da8c0d0db45b5fb",
+    "url": "/static/media/banner.8c6c7171.png"
+  },
+  {
+    "revision": "2120ba9bae76d9ff1be7424820acba06",
+    "url": "/static/media/fa-brands-400.2120ba9b.svg"
+  },
+  {
+    "revision": "45813ba62c5da9086efa217688da2996",
+    "url": "/static/media/fa-brands-400.45813ba6.eot"
+  },
+  {
+    "revision": "5c5f6302c00b7f5533d4368cc8feddec",
+    "url": "/static/media/fa-brands-400.5c5f6302.ttf"
+  },
+  {
+    "revision": "16f04cc3263785dc7f411e12c5869baa",
+    "url": "/static/media/fa-regular-400.16f04cc3.eot"
+  },
+  {
+    "revision": "39661f2a08f5ad003c93d3a7a553af84",
+    "url": "/static/media/fa-regular-400.39661f2a.svg"
+  },
+  {
+    "revision": "bbd1e7a4e8b9df3408636ca26fc49e7c",
+    "url": "/static/media/fa-regular-400.bbd1e7a4.ttf"
+  },
+  {
+    "revision": "1492b1ffaca4f7f3e2b36e1cbfc08ac0",
+    "url": "/static/media/fa-solid-900.1492b1ff.svg"
+  },
+  {
+    "revision": "881dc7cfff910fc3ebae32ebfed0e22d",
+    "url": "/static/media/fa-solid-900.881dc7cf.eot"
+  },
+  {
+    "revision": "bacbaa4247764ec86608f2fbe4e7347f",
+    "url": "/static/media/fa-solid-900.bacbaa42.ttf"
+  },
+  {
+    "revision": "74135066e3b145165b9f7e81b2e447fd",
+    "url": "/static/media/jacket.74135066.png"
+  },
+  {
+    "revision": "cd6459d783fbc985259a1bee1f3b9328",
+    "url": "/static/media/shirt.cd6459d7.png"
+  },
+  {
+    "revision": "158cde6381bfe81a751351e12ea5eda7",
+    "url": "/static/media/skirt.158cde63.png"
+  },
+  {
+    "revision": "1c7c73a1063f8e2b7104029e1705023c",
+    "url": "/static/media/t-shirt.1c7c73a1.png"
+  },
+  {
+    "revision": "c09ca42e4536224a12088a1260a63dab",
+    "url": "/static/media/tank-top.c09ca42e.png"
+  }
+]);
